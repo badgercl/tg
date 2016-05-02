0e79678d34b183eac60db1ce7c0072b4d2cc4cc4
Alfredo Cádiz <acadiz@gmail.com>
2016-05-02 11:04:28 -0300

    script to properly compile in OSX

---
+++ build_osx.sh
@@ -0,0 +1,2 @@
+export CFLAGS="-I/usr/local/include -I/usr/local/Cellar/readline/6.3.8/include -I/usr/local/include -I/usr/local/Cellar/readline/6.3.8/include" &&
+export LDFLAGS="-L/usr/local/lib -L/usr/local/Cellar/readline/6.3.8/lib -L/usr/local/lib -L/usr/local/Cellar/readline/6.3.8/lib" && ./configure && make
